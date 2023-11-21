01423cd0bd4b8c30495f7b5854c6a7b81f86fa24
juliafclima <juliafclima@hotmail.com>
2023-11-21 18:12:02 -0300

    subindo versao

--- trabalhog2/App.js
+++ trabalhog2/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return <Rotas />;
 }
 
-/* TAG: 4.8.0 */
+/* TAG: 4.9.0 */
 
 // Pressione Ctrl + Shift + P para abrir a paleta de comandos.
 // Digite 'Git: Push Tags' e pressione Enter.
