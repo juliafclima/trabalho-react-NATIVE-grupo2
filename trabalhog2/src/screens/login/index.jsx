997b9da73b7cf21807197076931a3d0381935eef
Pedro Horacio <129102505+Pedroh88@users.noreply.github.com>
2023-11-21 13:36:31 -0300

    Pagina Login

--- trabalhog2/src/screens/login/index.jsx
+++ trabalhog2/src/screens/login/index.jsx
@@ -1,28 +1,96 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react';
+import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {Logo} from '../../assets/login/Logo-Cod-Farma.png'
 
 export const Login = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate("Home")}>
-        <Text>
-          Ir para home
-        </Text>
-      </TouchableOpacity>
-    </View>
-  )
-};
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Image
+          source={Logo}
+        />
+      </View>
+
+      <View style={styles.container}>
+        <TextInput
+        style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => { }}
+        />
+
+        <TextInput
+        style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => { }}
+        />
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.btnText}>Entrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta gratuita</Text>
+        </TouchableOpacity>
+
+      </View>
+    </KeyboardAvoidingView>
+
+  );
+}
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "fff"
+  },
+
+  containerLogo: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+    paddingBottom: '50',
   },
-  botao: {
-    padding: 10,
+
+  imputed: {
+    backgroundColor:'FFF',
+    width:'90%',
+    marginBottom: '15',
+    color: '#222',
+    fontSize: '17',
+    borderRadius: '30',
+    paddingBottom: '10'
+  },
+
+  btnSubmit:{
+    backgroundColor: '#35AAFF',
+    width: '90%',
+    height: '45',
     alignItems: 'center',
-    borderRadius: 15,
-    width: '100%',
-    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    borderRadius: '30',
   },
-})
+
+  submitText:{ 
+    color: '#FFF',
+    fontSize: '18',
+  },
+
+  btnRegister:{
+  marginTop: '10',
+  },
+
+  registerText:{
+    color: '#FFF', 
+  }
+
+});
