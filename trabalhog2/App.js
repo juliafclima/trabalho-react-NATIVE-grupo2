635ab362d09280961ba74b9b6909742bb3dc260f
Hevelise - Liz <liz.souza.dias@gmail.com>
2023-11-21 16:39:24 -0300

    Subindo versão.

--- trabalhog2/App.js
+++ trabalhog2/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return <Rotas />;
 }
 
-/* TAG: 4.6.0 */
+/* TAG: 4.8.0 */
 
 // Pressione Ctrl + Shift + P para abrir a paleta de comandos.
 // Digite 'Git: Push Tags' e pressione Enter.
