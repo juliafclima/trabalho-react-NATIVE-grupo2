a7f4eb2aa11a1c2ad988b3389bc87a1295dbb835
juliafclima <juliafclima@hotmail.com>
2023-11-22 19:29:04 -0300

    tabs

--- trabalhog2/src/routes/index.jsx
+++ trabalhog2/src/routes/index.jsx
@@ -1,53 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from '../screens/home';
-import { Login } from '../screens/login';
-import { Produtos } from '../screens/produtos';
-import { Integrantes } from '../screens/integrantes';
-import { DetalhesProdutos } from '../screens/detalhesprodutos';
-import Cadastro from '../screens/cadastro';
-import { CadastrarProdutos } from '../screens/cadastrarProdutos';
-import { UpdateProdutos } from '../screens/updateProduto';
-
-const { Navigator, Screen } = createNativeStackNavigator();
+import { Stack } from './stack';
+import { Tab } from './tab';
 
 export const Rotas = () => {
   return (
     <NavigationContainer>
-      <Navigator>
-        <Screen
-          name='Login'
-          component={Login}
-          options={{ headerShown: false }} />
-        <Screen
-          name='Cadastro'
-          component={Cadastro}
-          options={{ headerShown: false }} />
-        <Screen
-          name='Home'
-          component={Home}
-          options={{ title: 'Página Principal', headerShown: false }} />
-        <Screen
-          name='Produtos'
-          component={Produtos}
-          options={{ title: 'Produtos' }} />
-        <Screen
-          name='DetalhesProdutos'
-          component={DetalhesProdutos}
-          options={{ title: 'Detalhes dos produtos' }} />
-        <Screen
-          name='CadastrarProdutos'
-          component={CadastrarProdutos}
-          options={{ title: 'Cadastro de produtos' }} />
-        <Screen
-          name='UpdateProdutos'
-          component={UpdateProdutos}
-          options={{ title: 'Editar produto' }} />
-        <Screen
-          name='Integrantes'
-          component={Integrantes}
-          options={{ title: 'Integrantes' }} />
-      </Navigator>
+      {/* <Stack /> */}
+      <Tab />
     </NavigationContainer>
   );
 };
---
+++ trabalhog2/src/routes/tab.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+import { Home } from "../screens/Home";
+import { Login } from "../screens/login";
+import Cadastro from "../screens/cadastro";
+import { Produtos } from "../screens/produtos";
+import { Integrantes } from "../screens/integrantes";
+import { Stack } from "./stack";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export const Tab = () => {
+  return (
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: "#3498db",
+        inactiveTintColor: "#bdc3c7",
+      }}
+    >
+      <Screen
+        name="Stack"
+        component={Stack}
+        options={{
+          tabBarLabel: "Inicio",
+          header: () => null,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Produtos"
+        component={Produtos}
+        options={{
+          tabBarLabel: "Produtos",
+          header: () => null,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Integrantes"
+        component={Integrantes}
+        options={{
+          tabBarLabel: "Integrantes",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+};
